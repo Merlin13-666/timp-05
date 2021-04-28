dffce8f5b74c5ff823a852b4d74d61b87e8f3144
Merlin13-666 <danya-a@inbox.ru>
2021-04-28 08:35:31 +0300

    first

---
+++ banking/Account.h
@@ -0,0 +1,27 @@
+#pragma once
+class Account {
+ public:
+  Account(int id, int balance);
+  virtual ~Account();
+
+  // Virtual to test.
+  virtual int GetBalance() const;
+
+  // Virtual to test.
+  virtual void ChangeBalance(int diff);
+
+  // Virtual to test.
+  virtual void Lock();
+
+  // Virtual to test.
+  virtual void Unlock();
+  int id() const { return id_; }
+  
+  boll operator==(const Account)const
+  {return true;}
+
+ private:
+  int id_;
+  int balance_;
+  bool is_locked_;
+};
